478dfe8537ca3eb500587f0985f436b75ae0e150
stpafk <117909784+stpafk@users.noreply.github.com>
2023-02-13 12:01:41 +0000

    git push

--- calculator/main.js
+++ calculator/main.js
@@ -1,14 +1,11 @@
-let firstNum = 0
-let secondNum = 0
-let operator = 0
+let operations = []
 
 const inputInterface = document.querySelector('#input');
 
 const clearButton = document.querySelector('#Clear');
 clearButton.addEventListener('click', function (){
     inputInterface.textContent = 0;
-    firstNum = 0
-    secondNum = 0
+    operations = []
 });
 
 const intButtons = document.querySelectorAll('.calculator .row button');
@@ -31,45 +28,51 @@ const divisionButton = document.querySelector("#Division");
 
 equalButton.addEventListener('click', function(){
 
-    secondNum = document.querySelector('#input').innerHTML;
-    if (operator === "+"){
-        inputInterface.textContent = parseFloat(firstNum) + parseFloat(secondNum);
+    operations.push(parseFloat(document.querySelector('#input').innerHTML));
+    let result = operations[0];
+    for (let i=1;i<operations.length;i+=2) {
+        let operator = operations[i];
+        let secondOperand = operations[i+1];
+        if (operator === '+') {
+            result += secondOperand;
+        }
+        else if (operator === '-') {
+            result -= secondOperand;
+        }
+        else if (operator === '*') {
+            result *= secondOperand;
+        }
+        else if (operator === '/') {
+            result /= secondOperand;
+        }
     }
-    else if (operator === "-"){
-        inputInterface.textContent = parseFloat(firstNum) - parseFloat(secondNum);
-    }
-    else if (operator === '*'){
-        inputInterface.textContent = parseFloat(firstNum) * parseFloat(secondNum);
-    }
-    else if (operator === '/'){
-        inputInterface.textContent = parseFloat(firstNum) / parseFloat(secondNum);
-    }
+    inputInterface.textContent = result;
+    operations = []
 });
 
 plusButton.addEventListener('click', function(){
 
-    firstNum = parseInt(document.querySelector('#input').innerHTML);
+    operations.push(parseFloat(document.querySelector('#input').innerHTML));
+    operations.push('+');
     inputInterface.textContent = "";
-    operator = "+"
 });
 
 minusButton.addEventListener('click', function(){
 
-    firstNum = document.querySelector('#input').innerHTML;
+    operations.push(parseFloat(document.querySelector('#input').innerHTML));
+    operations.push('-');
     inputInterface.textContent = "";
-    operator = "-"
 });
 
 timesButton.addEventListener('click', function(){
 
-    firstNum = parseInt(document.querySelector('#input').innerHTML);
+    operations.push(parseFloat(document.querySelector('#input').innerHTML));
+    operations.push('*');
     inputInterface.textContent = "";
-    operator = "*"
 });
 
 divisionButton.addEventListener('click', function(){
-
-    firstNum = parseInt(document.querySelector('#input').innerHTML);
+    operations.push(parseFloat(document.querySelector('#input').innerHTML));
+    operations.push('/');
     inputInterface.textContent = "";
-    operator = "/"
 });
